0005a99599a93ea57a59c69f0efd56c843b73af6
Benjamin Hoffstadt <58375543+ididitmyway@users.noreply.github.com>
2020-11-10 21:45:04 +0100

    fix path ps1

--- loopPostreSQL.exe.ps1
+++ loopPostreSQL.exe.ps1
@@ -1,11 +1,11 @@
 for ($i = 0; $i -lt 100; $i++) {
-    Start-Process -FilePath 'C:\Bens\Code\Postgress\bin\Debug\netcoreapp3.0\PostgreSQL.exe'  
+    Start-Process -FilePath '.\netcoreapp3.0\PostgreSQL.exe'  
 }
 Stop-Process -Name PostgreSQL
 Start-Process docker -ArgumentList "kill some-postgres-11"
 Start-Process docker -ArgumentList "start some-postgres-11"
 for ($i = 0; $i -lt 100; $i++) {
-    Start-Process -FilePath 'C:\Bens\Code\Postgress\bin\Debug\netcoreapp3.0\PostgreSQL.exe'  
+    Start-Process -FilePath '.\netcoreapp3.0\PostgreSQL.exe'  
 }
 Start-Process docker -ArgumentList "kill some-postgres-11"
 Start-Process docker -ArgumentList "start some-postgres-11"
